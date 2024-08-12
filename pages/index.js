f4adaff86762738e705d5abbff8f7650bf4c25b8
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-08-12 15:19:02 +0300

    Location Detection

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,27 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.scss";
 import Header from "../components/header"
 import Footer from "@/components/footer";
-export default function Home() {
+import axios from 'axios';
+export default function Home({country}) {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <Header country={country}/>
+      <Footer country={country}/>
     </div>
   );
 }
+export async function getServerSideProps() {
+    let data = await axios.get('https://api.ipregistry.co/?key=lffcfrgnynpxbvyj')
+      .then((res) => {
+        return res.data.location.country;
+      })
+   .catch ((err) =>{
+    console.log(err);})
+    // Повертаємо порожній рядок або обробляємо помилку іншим чином
+    return {
+      props: {
+        country: {name:data.name,flag:data.flag.emojitwo}
+      }
+    };
+  }
+
