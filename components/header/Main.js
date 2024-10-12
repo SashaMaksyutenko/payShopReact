d52b253db73f46c57cf68bb8cf156d48a9bea387
Sasha Maksyutenko <sasha.maksyutenko@gmail.com>
2024-10-12 15:23:55 +0300

    Browse page filters, adding search

--- components/header/Main.js
+++ components/header/Main.js
@@ -1,27 +1,50 @@
-import Link from 'next/link'
-import styles from './styles.module.scss'
+import Link from "next/link";
+import styles from "./styles.module.scss";
 import { RiSearch2Line } from "react-icons/ri";
 import { FaOpencart } from "react-icons/fa";
 import { useSelector } from "react-redux";
-export default function Main () {
+import { useState } from "react";
+import { useRouter } from "next/router";
+export default function Main({ searchHandler }) {
+  const router = useRouter();
+  const [query, setQuery] = useState(router.query.search || "");
   const { cart } = useSelector((state) => ({ ...state }));
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (router.pathname !== "/browse") {
+      if (query.length > 1) {
+        router.push(`/browse?search=${query}`);
+      }
+    } else {
+      searchHandler(query);
+    }
+  };
   return (
     <div className={styles.main}>
       <div className={styles.main__container}>
-        <Link href='/' className={styles.logo}>
-          <img src='../../../logo.png' alt=''></img>
+        <Link legacyBehavior href="/">
+          <a className={styles.logo}>
+            <img src="../../../logo.png" alt="" />
+          </a>
         </Link>
-        <div className={styles.search}>
-          <input type="text" placeholder="Search..."></input>
-          <div className={styles.search__icon}>
-            <RiSearch2Line/>
-          </div>
-        </div>
-        <Link href='/cart'className={styles.cart}>
-        <FaOpencart/>
-        <span>0</span>
+        <form onSubmit={(e) => handleSearch(e)} className={styles.search}>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className={styles.search__icon}>
+            <RiSearch2Line />
+          </button>
+        </form>
+        <Link legacyBehavior href="/cart">
+          <a className={styles.cart}>
+            <FaOpencart />
+            <span>0</span>
+          </a>
         </Link>
       </div>
     </div>
-  )
+  );
 }
--- components/header/index.js
+++ components/header/index.js
@@ -1,13 +1,13 @@
-import styles from "./styles.module.scss"
-import Ad from "./Ad"
-import Top from "./Top"
-import Main from "./Main"
-export default function Header({country}) {
+import Ad from "./Ad";
+import Main from "./Main";
+import styles from "./styles.module.scss";
+import Top from "./Top";
+export default function Header({ country, searchHandler }) {
   return (
     <header className={styles.header}>
-      <Ad/>
-      <Top country={country}/>
-      <Main/>
+      <Ad />
+      <Top country={country} />
+      <Main searchHandler={searchHandler} />
     </header>
-  )
+  );
 }
